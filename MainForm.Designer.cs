7ce014db542123bedea1ab1ac8174f6d2a32885a
Meyer137n <112759280+Meyer137n@users.noreply.github.com>
2024-10-16 22:35:32 +0700

    Базовая программа с генерацией 5 фигур и 1 кнопкой
    -Генерирует 5 фигур
    -Кнопка для генерации фигур

---
+++ MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System.Drawing;
+using System;
+
+namespace CG_LAB2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonDeffaultPosition = new System.Windows.Forms.Button();
+            //this.buttonAlgorithm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonDeffaultPosition
+            // 
+            this.buttonDeffaultPosition.Location = new System.Drawing.Point(60, 50);
+            this.buttonDeffaultPosition.Name = "buttonDeffaultPosition";
+            this.buttonDeffaultPosition.Size = new System.Drawing.Size(183, 30);
+            this.buttonDeffaultPosition.TabIndex = 0;
+            this.buttonDeffaultPosition.Text = "Начльная позиция";
+            this.buttonDeffaultPosition.UseVisualStyleBackColor = true;
+            this.buttonDeffaultPosition.Click += new System.EventHandler(this.buttonDeffaultPosition_Click);
+            //
+            // buttonAlgorithm
+            //
+/*            this.buttonAlgorithm.Location = new System.Drawing.Point(60, 100);
+            this.buttonAlgorithm.Name = "buttonAlgorithm";
+            this.buttonAlgorithm.Size = new System.Drawing.Size(183, 30);
+            this.buttonAlgorithm.TabIndex = 0;
+            this.buttonAlgorithm.Text = "Применить алгоритм";
+            this.buttonAlgorithm.UseVisualStyleBackColor = true;
+            this.buttonAlgorithm.Click += new System.EventHandler(this.buttonApplyAlgorithm_Click);*/
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 572);
+            this.Controls.Add(this.buttonDeffaultPosition);
+            //this.Controls.Add(this.buttonAlgorithm);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Афинные преобразования";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDeffaultPosition;
+        //private System.Windows.Forms.Button buttonAlgorithm;
+    }
+}
+
+
---
+++ MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CG_LAB2
+{
+    public partial class MainForm : Form
+    {
+        private float[,] proection;
+        private int cenX;
+        private int cenY;
+        private Graphics _graphics;
+        private Random _random = new Random();
+        private List<float[,]> polygons;
+
+        public MainForm() => InitializeComponent();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            cenX = Size.Width / 2 - 700;
+            cenY = Size.Height / 2 - 250;
+
+            // Настройка матрицы проекции для изометрического проецирования
+            float[,] p =
+            {
+                { 1, 0, 0, 0},
+                { 0, -1, 0, 0},
+                { -(float)(Math.Cos(Math.PI/4))/2, (float)(Math.Cos(Math.PI/4))/2, 0, 0},
+                { cenX, cenY, 0, 1}
+            };
+            proection = p;
+
+            GeneratePolygons();
+            DrawPolygons();
+        }
+
+        // Генерация случайных полигонов (от 3 до 6 вершин) с постепенным смещением вправо
+        private void GeneratePolygons()
+        {
+            polygons = new List<float[,]>();
+            float shiftX = 0;  // Переменная для смещения по оси X
+
+            for (int i = 0; i < 6; i++) // Генерируем 5 полигонов
+            {
+                int verticesCount = _random.Next(3, 7); // Случайное количество рёбер от 3 до 6
+                float[,] polygon = new float[verticesCount, 4];
+
+                // Определение вершин на окружности с учётом смещения
+                for (int j = 0; j < verticesCount; j++)
+                {
+                    double angle = 2 * Math.PI * j / verticesCount;
+                    polygon[j, 0] = (float)(50 * Math.Cos(angle)) + shiftX;  // x-координата со смещением
+                    polygon[j, 1] = (float)(50 * Math.Sin(angle));           // y-координата
+                    polygon[j, 2] = _random.Next(-20, 20);                   // z-координата (случайная высота)
+                    polygon[j, 3] = 1;                                      // однородная координата
+                }
+                polygons.Add(polygon);
+
+                // Увеличиваем смещение для следующей фигуры
+                shiftX += 70;  // Измените это значение, чтобы контролировать размер смещения
+            }
+        }
+
+        // Умножение матриц
+        private float[,] Mult(float[,] X, float[,] Y)
+        {
+            float[,] result = new float[X.GetLength(0), Y.GetLength(1)];
+            for (int i = 0; i < X.GetLength(0); i++)
+                for (int j = 0; j < Y.GetLength(1); j++)
+                    for (int k = 0; k < Y.GetLength(0); k++)
+                        result[i, j] += X[i, k] * Y[k, j];
+            return result;
+        }
+
+        // Отрисовка сгенерированных полигонов
+        private void DrawPolygons()
+        {
+            _graphics = CreateGraphics();
+
+            foreach (var polygon in polygons)
+            {
+                float[,] projectedPolygon = Mult(polygon, proection);
+                PointF[] points = new PointF[polygon.GetLength(0)];
+
+                for (int i = 0; i < polygon.GetLength(0); i++)
+                {
+                    points[i] = new PointF(projectedPolygon[i, 0], projectedPolygon[i, 1]);
+                }
+
+                // Нарисовать полигон в 2D после проекции
+                _graphics.DrawPolygon(Pens.Blue, points);
+            }
+        }
+
+        // Сброс на стандартные позиции и вызов отрисовки
+        private void buttonDeffaultPosition_Click(object sender, EventArgs e)
+        {
+            GeneratePolygons();
+            DrawPolygons();
+        }
+    }
+}
